81ab98643b2a2fc485af3a8a59256d63c1407f6d
mta3004 <130672068+mta3004@users.noreply.github.com>
2023-08-09 22:00:56 +0700

    Update content.js

--- src/utils/content.js
+++ src/utils/content.js
@@ -1,24 +1,44 @@
 export const accordionData = [
     {
       title: 'Rice leaf folder',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
+      content: `Intensive fertilizer usage promotes the rapid proliferation
+      of the insect. The presence of high humidity and shaded regions within the field,
+      along with the existence of grassy weeds from both rice fields and surrounding
+      borders, contributes to the pest’s growth. Increased rice cultivation with irrigation
+      systems, multiple rice cropping, and insecticide use leading to resurgences play
+      significant roles in the abundance of these insects. Rice leaf folders can be found
+      in all rice environments but are more prevalent during rainy seasons. They tend
+      to inhabit shady areas and regions where rice is heavily fertilized. In tropical rice
+      regions, they remain active throughout the year, while in temperate countries, their
+      activity spans from May to October. Examine the plant for the following indications:
+      – Longitudinal and transparent whitish streaks on affected leaves.
+      7
+      – Tubular folded leaves.
+      – Leaf tips are sometimes attached to the lower part of the leaf.
+      – Fields heavily infested may show scorching with numerous folded leaves`
     },
     {
       title: 'Rice leaf blast',
-      content: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia veniam
-      reprehenderit nam assumenda voluptatem ut. Ipsum eius dicta, officiis
-      quaerat iure quos dolorum accusantium ducimus in illum vero commodi
-      pariatur? Impedit autem esse nostrum quasi, fugiat a aut error cumque
-      quidem maiores doloremque est numquam praesentium eos voluptatem amet!
-      Repudiandae, mollitia id reprehenderit a ab odit!`
+      content: `Blast disease can manifest wherever there are blast spores.
+      It commonly appears in regions characterized by low soil moisture, frequent and
+      extended rain showers, and cool daytime temperatures. Initial lesions are recognizable by their white to gray-green
+      appearance, accompanied by dark green borders. As they age, these lesions take on
+      an elliptical or spindle-like shape with whitish-to-gray centers and red-to-brownish
+      or necrotic borders. Another symptom of leaf blast resembles a diamond shape,
+      wider in the center and tapering towards the ends. In severe cases, lesions can
+      merge, leading to the destruction of entire leaves.`
     },
     {
       title: 'Brown spot',
-      content: `Sapiente expedita hic obcaecati, laboriosam similique omnis architecto ducimus magnam accusantium corrupti
-      quam sint dolore pariatur perspiciatis, necessitatibus rem vel dignissimos
-      dolor ut sequi minus iste? Quas?`
+      content: ` The disease tends to emerge in regions with elevated relative humidity (between 86% and 100%) and temperatures ranging from 16°C to
+      36°C. It is frequently found in soil lacking proper nutrients or in soils that accu-
+      mulate harmful substances. Infected seedlings display small, circular lesions in shades of
+      yellow-brown or brown, potentially encircling the coleoptile and distorting primary
+      and secondary leaves. As the crop reaches the tillering stage, lesions emerge on the
+      leaves. Initially, these lesions are small, circular, and dark brown to purple-brown.
+      Once fully developed, they take on a circular to oval shape with a light brown to
+      gray center, encompassed by a reddish-brown margin formed by the fungi’s toxin.
+      On susceptible varieties, lesions can be 5-14 mm long, leading to leaf wilting. On
+      resistant varieties, the lesions appear brown and are the size of pinheads.`
     }
   ];
